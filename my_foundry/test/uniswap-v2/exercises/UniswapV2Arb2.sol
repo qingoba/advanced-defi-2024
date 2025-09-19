004803ac322c4b8f101b6c30f4155832a851f196
Yuqing Liu <yuqingk@amazon.com>
2025-09-19 23:10:14 +0800

    [Arb2 Ex] initial solution

--- my_foundry/test/uniswap-v2/exercises/UniswapV2Arb2.sol
+++ my_foundry/test/uniswap-v2/exercises/UniswapV2Arb2.sol
@@ -59,6 +59,23 @@ contract UniswapV2Arb2 {
         });
 
         // (2) 调用 flashswap 并回调套利逻辑
+        //     准备函数参数, 具体套利逻辑在回调函数内操作, 包括利润计算和转移
+        FlashSwapData memory params = FlashSwapData({
+            caller: msg.sender,
+            pair0: pair0,
+            pair1: pair1,
+            isZeroForOne: isZeroForOne,
+            amountIn: amountIn,
+            amountOut: amountOut,
+            minProfit: minProfit
+        });
+        bytes memory data = abi.encode(params);
+        IUniswapV2Pair(pair0).swap({
+            amount0Out: isZeroForOne ? 0 : amountOut,
+            amount1Out: isZeroForOne ? amountOut : 0,
+            to: address(this),          // 回调当前合约的 uniswapV2Call 函数
+            data: data
+        });
 
     }
 
@@ -70,6 +87,51 @@ contract UniswapV2Arb2 {
     ) external {
         // Write your code here
         // Don’t change any other code
+
+        // (3) 解析参数, 执行 pair1 兑换
+        //     由于我们直接使用 Pair 合约而不是 Router 合约进行交互, 因此要先转账, 从 pair0 从借出来的钱在当前合约中
+        //     这里就涉及到一个很关键的问题了, 转账是如何进行的? 是 token 合约负责的转账.
+        //     把借出的代币通过 ERC20 合约从当前合约转到 pair1 合约, token 的地址直接从 pair0 中取出来
+        FlashSwapData memory params = abi.decode(data, (FlashSwapData));
+
+        // (3.1) tokenOutPair0 是表示 flashswap 借出的代币地址
+        //       tokenInPair0 表示 flashswap 应该归还的代币地址
+        address tokenOutPair0;
+        address tokenInPair0;
+        if (params.isZeroForOne) {
+            tokenOutPair0 = IUniswapV2Pair(params.pair0).token1();
+            tokenInPair0 = IUniswapV2Pair(params.pair0).token0();
+        } else {
+            tokenOutPair0 = IUniswapV2Pair(params.pair0).token0();
+            tokenInPair0 = IUniswapV2Pair(params.pair0).token1();
+        }
+
+        // (3.2) 将借出的代币转给 pair1 合约, 换出另一代币
+        //       Pair 合约的 swap 函数并不计算能换出多少代币, 这个计算一般是在 Router 合约中的
+        //       Pair 合约只检查符合手续费要求, 然后就把给定数量的代币转出, 并不管滑点之类的信息
+        //       所以这里我们需要自己计算能够换出的数量, 然后将该数量传入 swap 函数, 要计算这个数量, 还要区分清楚 token0 和 token1
+        //       
+        //       我们转进去的代币是 tokenOutPair0, 需要知道 tokenOutPair0 在 pair1 里是 token0 还是 token1
+        bool isToken0InPair1 = IUniswapV2Pair(params.pair1).token0() == tokenOutPair0;
+        (uint256 reserve0, uint256 reserve1, ) = IUniswapV2Pair(params.pair1).getReserves();
+        uint256 amountOutPair1 = getAmountOut({
+            amountIn: params.amountOut,
+            reserveIn: isToken0InPair1 ? reserve0 : reserve1,
+            reserveOut: isToken0InPair1 ? reserve1 : reserve0
+        });
+        IERC20(tokenOutPair0).transfer(params.pair1, params.amountOut);
+        IUniswapV2Pair(params.pair1).swap({
+            amount0Out: isToken0InPair1 ? 0 : amountOutPair1,
+            amount1Out: isToken0InPair1 ? amountOutPair1 : 0,
+            to: address(this),
+            data: ""
+        });
+
+        // (4) 非常值得注意的是, 从 pair1 中换出的代币数量就是 amoutOutPair1, 存在当前合约中
+        //     根据 amountInPair0 计算利润, 最后把 amountInPair0 还给 pair0 合约, 剩下的返回给用户
+        require(amountOutPair1 - params.amountIn >= params.minProfit, "profit is less than minProfit");
+        IERC20(tokenInPair0).transfer(params.pair0, params.amountIn);
+        IERC20(tokenInPair0).transfer(params.caller, amountOutPair1 - params.amountIn);
     }
 
     function getAmountOut(
